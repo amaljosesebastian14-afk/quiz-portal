2752cd9affdd54cbaf79fdb05ef797fb42a7bda9
amaljosesebastian14-afk <amaljosesebastian14@gmail.com>
2026-06-25 22:32:21 +0530

    Update Firebase config for Render

--- backend/app.js
+++ backend/app.js
@@ -41,7 +41,7 @@ async function startServer() {
 
         //await connectRedis();
 
-        const PORT = process.env.PORT || 3000;
+        const PORT = process.env.PORT || 5000;
 
         app.listen(PORT, () => {
             console.log(`Server Running On Port ${PORT}`);
