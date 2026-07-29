5b669ef4d3f4ae6fd75395311ab6f72d13e9d1f1
amaljosesebastian14-afk <amaljosesebastian14@gmail.com>
2026-07-30 01:07:10 +0530

    Add debug logging

--- frontend/js/exam.js
+++ frontend/js/exam.js
@@ -21,13 +21,18 @@ try {
         ? getCurrentLang()
         : "en";
 
+    const url =
+    `https://quiz-portal-1lia.onrender.com/api/exam/start/${examId}?userId=${user._id}&lang=${lang}`;
+
+    console.log("DEBUG - Fetch URL:", url);
+
     const response =
-    await fetch(
-        `https://quiz-portal-1lia.onrender.com/api/exam/start/${examId}?userId=${user._id}&lang=${lang}`
-    );
+    await fetch(url);
 
     const data =
     await response.json();
+
+    console.log("DEBUG - First question received:", data.questions ? data.questions[0] : data);
 
     if (!data.success) {
 
